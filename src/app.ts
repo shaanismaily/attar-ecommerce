9a2a1977f87663317721268dc005bc3a1cbbf563
Muhammad Shaan <shaanrazaismaily@gmail.com>
2026-07-25 19:07:47 +0530

    add order routes to app; integrate orderRouter for API versioning

--- src/app.ts
+++ src/app.ts
@@ -16,9 +16,11 @@ app.use(cookieParser())
 
 import userRouter from "./routes/user.routes.js"
 import productRouter from "./routes/product.routes.js"
+import orderRouter from "./routes/order.routes.js"
 
 
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1", productRouter)
+app.use("/api/v1", orderRouter)
 
 export { app };
