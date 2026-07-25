79337e04f8d9aaf0c0c7d7ba692e646fd2f74841
Muhammad Shaan <shaanrazaismaily@gmail.com>
2026-07-25 19:08:26 +0530

    add address model and schema; implement address management with pre-save logic

---
+++ src/models/address.model.ts
@@ -0,0 +1,96 @@
+import mongoose, { Schema, Types, Model } from "mongoose";
+
+interface IAddress {
+    user: Types.ObjectId;
+    fullName: string;
+    phone: string;
+    street: string;
+    landmark: string;
+    city: string;
+    state: string;
+    country: string;
+    zipCode: string;
+    addressType: string;
+    isDefault: boolean;
+
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+
+const addressSchema = new Schema<IAddress, Model<IAddress>>({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 100
+    },
+    phone: {
+        type: String,
+        required: true,
+        match: /^[6-9]\d{9}$/
+    },
+    street: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 300
+    },
+    landmark: {
+        type: String,
+        trim: true,
+        maxlength: 100
+    },
+    city: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 100
+    },
+    state: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 100
+    },
+    country: {
+        type: String,
+        required: true,
+        trim: true,
+        default: "India"
+    },
+    zipCode: {
+        type: String,
+        match: /^\d{6}$/,
+        required: true,
+    },
+    addressType: {
+        type: String,
+        enum: ["home", "work", "other"],
+        required: true,
+    },
+    isDefault: {
+        type: Boolean,
+        default: false
+    }
+}, { timestamps: true });
+
+
+addressSchema.pre("save", async function() {
+  if (this.isDefault) {
+    await mongoose.model("Address").updateMany(
+      { user: this.user, _id: { $ne: this._id } },
+      { isDefault: false }
+    );
+  }
+})
+
+addressSchema.index({ user: 1, isDefault: 1 });
+addressSchema.index({ city: 1, state: 1 });
+
+export const Address = mongoose.model<IAddress>("Address", addressSchema)
