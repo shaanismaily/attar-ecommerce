a4a22a9a632ee5120e6034d6ecfa5a8bffa156d9
Muhammad Shaan <shaanrazaismaily@gmail.com>
2026-07-26 16:43:21 +0530

    add cart model and schema; implement cart item management with timestamps

---
+++ src/models/cart.model.ts
@@ -0,0 +1,53 @@
+import mongoose, { Schema, Types, Model } from "mongoose";
+
+interface ICartItem {
+    product: Types.ObjectId;
+    variant: Types.ObjectId;
+
+    quantity: number;
+    priceAtAddition: number;
+}
+
+interface ICart {
+    user: Types.ObjectId;
+    items: ICartItem[];
+
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+const cartItemSchema = new Schema<ICartItem>({
+    product: {
+        type: Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+    },
+    variant: {
+        type: Schema.Types.ObjectId,
+        ref: "Variant",
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        min: 1
+    },
+    priceAtAddition: {
+        type: Number,
+        required: true,
+        min: 0
+    }
+})
+
+const cartSchema = new Schema<ICart, Model<ICart>>({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+        unique: true
+    },
+    items: [cartItemSchema]
+}, { timestamps: true })
+
+
+export const Cart = mongoose.model<ICart>("Cart", cartSchema)
