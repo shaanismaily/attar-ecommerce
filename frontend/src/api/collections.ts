714651543bd9a2760338dd1c08fb0d65a753e925
Muhammad Shaan <shaanrazaismaily@gmail.com>
2026-08-03 20:37:15 +0530

    refactor: update type names for consistency and improve API method implementations

--- frontend/src/api/collections.ts
+++ frontend/src/api/collections.ts
@@ -20,7 +20,7 @@ type ApiResponse<T> = {
     success: boolean;
 };
 
-type categoryData = {
+type CategoryData = {
     name: string;
     description: string;
     isActive: boolean;
@@ -37,27 +37,42 @@ type UpdateCategoryFields = {
     isActive?: boolean;
 };
 
-export type updateCategoryData = RequireAtLeastOne<UpdateCategoryFields>;
+export type UpdateCategoryData = RequireAtLeastOne<UpdateCategoryFields>;
 
-export const getAllCollections = (config?: AxiosRequestConfig) =>
-    client.get<ApiResponse<Collection[]>>("/categories", config);
-
-export const getCollectionById = () => {
-    client.get("/categories/:categoryId")
+export const getAllCollections = (config?: AxiosRequestConfig) => {
+    return client.get<ApiResponse<Collection[]>>("/categories", config);
 }
 
-export const createCategory = (data: categoryData) => {
-    client.post("/admin/categories", data)
+export const getCollectionById = (categoryId: string) => {
+    return client.get<ApiResponse<Collection>>(`/categories/${categoryId}`)
 }
 
-export const updateCategory = (data: updateCategoryData) => {
-    client.patch("/admin/categories/:categoryId", data)
+export const createCategory = (data: CategoryData) => {
+    const formData = new FormData();
+
+    formData.append("name", data.name);
+    formData.append("description", data.description);
+    formData.append("isActive", String(data.isActive));
+    formData.append("image", data.image);
+
+    return client.post<ApiResponse<Collection>>("/admin/categories", formData);
+};
+
+export const updateCategory = (categoryId: string, data: UpdateCategoryData) => {
+    return client.patch<ApiResponse<Collection>>(`/admin/categories/${categoryId}`, data)
 }
 
-export const updateImage = (image: File) => {
-    client.patch("/admin/categories/:categoryId/image", image)
-}
+export const updateImage = (image: File, categoryId: string) => {
+    const formData = new FormData();
 
-export const deleteCategory = () => {
-    client.delete("/admin/categories/:categoryId")
+    formData.append("image", image);
+
+    return client.patch(
+        `/admin/categories/${categoryId}/image`,
+        formData
+    );
+};
+
+export const deleteCategory = (categoryId: string) => {
+    return client.delete(`/admin/categories/${categoryId}`)
 }
