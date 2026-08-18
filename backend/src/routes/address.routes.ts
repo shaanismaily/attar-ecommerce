69d3247199f6593c8f7bfd62f24952f64066d9ee
Shan Mohd <shaanrazaismaily@gmail.com>
2026-08-18 19:18:30 +0530

    feat: enhance address routes by adding missing address retrieval and update functionalities

--- backend/src/routes/address.routes.ts
+++ backend/src/routes/address.routes.ts
@@ -1,15 +1,27 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { createAddress, deleteAddress, getAddresses } from "../controllers/address.controller.js";
+import { 
+    createAddress, 
+    deleteAddress, 
+    getAddressById, 
+    getAddresses, 
+    setDefaultAddress, 
+    updateAddress 
+} from "../controllers/address.controller.js";
 
 const router = Router();
 
 router.use(verifyJWT)
 
-router
-    .post("/", createAddress)
-    .get("/", getAddresses)
+router.route("/")
+    .post(createAddress)
+    .get(getAddresses)
 
-router.delete("/:addressId", deleteAddress)
+router.route("/:addressId")
+    .get(getAddressById)
+    .patch(updateAddress)
+    .delete(deleteAddress)
+
+router.patch("/:addressId/default", setDefaultAddress)
 
 export default router;
